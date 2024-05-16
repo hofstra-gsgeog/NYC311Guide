2b916e7011d85ba740e5c1b81c17c4603e8385da
nicdanluc <108090213+nicdanluc@users.noreply.github.com>
2024-05-16 09:02:36 -0400

    Updated Instructions311Floods.R
    Added instructions for filtering year and borough in R, instead of NYCOD's site (not recommended)

--- Instructions311Floods.R
+++ Instructions311Floods.R
@@ -15,7 +15,9 @@ library(writexl)
 
 # 311 Data Page: https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9/about_data
 
-# Warning: Imports from NYC Open Data come w/ limit of 776,874 rows.
+# IMPORTANT: I used NYC OpenData's online query tool to filter for only Queens data from 2023. I would recommend doing this to shrink the file size before importing, but alternatively filtering through R is possible (see below).
+
+# Warning: Imports from NYC OpenData come w/ limit of 776,874 rows.
   # If you want to import your NYCOD query thru the API Endpoint (instead of downloading), you will need to add "&$limit=[# of queried rows + 1]" to the end of the URL.
   # Downloading .csv = easier, but takes more time
 # QUERY: Queens only, 1/1/23 @ 12:00 AM to 12/31/23 @ 11:59:59PM
@@ -35,10 +37,16 @@ reports311 <- read.csv("[file or URL]")
 rep_floods <- reports311 %>%
   filter(Agency.Name=="Department of Transportation" | Agency.Name=="Department of Environmental Protection") %>%
   filter(Descriptor=="Highway Flooding (SH)" | Descriptor=="Street Flooding (SJ)" | Descriptor=="Flooding on Highway" | Descriptor=="Flooding on Street") %>%
+#Run below with above code only if you didn't query for your Borough on NYCOD's website first.
+  # filter(Borough=="[Your Borough Here]")
+
   rep_floods <- rep_floods %>%
   mutate(Y = substring(rep_floods$Created.Date,9,10),
          M = substring(rep_floods$Created.Date,1,2),
          D = substring(rep_floods$Created.Date,4,5))
+#Run below only if you didn't query for 2023 on NYCOD's website first.
+  # rep_floods <- rep_floods %>%  
+    # filter(Y=="23")
 
   # FLOOD INDICATORS 
 rep_indicators <- reports311 %>%
